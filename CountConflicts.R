eca2aa7c32c70ee5ad92bbf1cb6c85ba05dd036d
Jacqueline <jacquelinemay29@gmail.com>
2017-08-30 23:16:30 -0400

    CountConflicts

---
+++ CountConflicts.R
@@ -0,0 +1,23 @@
+# Function: CountConflicts #
+# Purpose:  Counts how many BIN conflicts there for a given taxonomic level and 
+#           returns a vector of the BIN names.
+CountConflicts <- function(x, y) {
+  # x = Dataframe or datatable to apply function to.
+  # y = Name of column of interest (the column that contains counts of order/
+  #     family/genus/species per BIN).
+  
+  # Convert to dataframe format.
+  x <- as.data.frame(x)
+  # Which BINs have a value greater than 1/
+  conflicts <- which(x[, y] > 1)
+  # Subset these BINs.
+  conflicts <- x[conflicts, ]
+  # We only want unique BIN URIs.
+  conflicts <- unique(conflicts$bin_uri)
+  # How many are there?
+  num_conflicts <- length(conflicts)
+  # Let the user know!
+  print(paste0("There are ", num_conflicts, " BIN conflicts!"))
+  # Return a vector of the names of conflicted BINs.
+  return(conflicts)
+}

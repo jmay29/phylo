644b550b2d82979e7be9ed59a51e9c9d1621e2de
Jacqueline <jacquelinemay29@gmail.com>
2017-10-10 21:52:55 -0400

    Added functions.

--- Sec2_FishBaseTraits_V2.R
+++ Sec2_FishBaseTraits_V2.R
@@ -6,8 +6,8 @@
 
 # Contributions & Acknowledgements #
 # Matt Orton (https://github.com/m-orton/R-Scripts) for contributions to the 
-# latitude trait section (lines 52-62).
-# Adapted lines 196-197 and 315-319 from code shared in Stack Overflow 
+# latitude trait section (lines 53-63).
+# Adapted lines 177-178 and 235-238 from code shared in Stack Overflow 
 # discussion:
 # Author: https://stackoverflow.com/users/403310/matt-dowle.
 # https://stackoverflow.com/questions/13273833/merging-multiple-data-table.
@@ -46,6 +46,7 @@ library(rfishbase)
 # Load the function(s) designed for this script:
 source("GetTraitSpecificDataBIN.R")
 source("GetTraitSpecificData.R")
+source("GetTraitInfo.R")
 
 ################################################################################
 
@@ -70,20 +71,15 @@ dfLatitudeSpecies[, median_lat := median(abs_lat_num), keyby = bin_uri]
 # TRAIT: Latitude.
 # Use the GetTraitSpecificDataBIN function to obtain latitude data on a per 
 # BIN basis.
-# TEST 1: Does trait have data for at least 300 species?
 # Note: I am also converting to datatable format in process.
-dfLatitude <- setDT(GetTraitSpecificDataBIN(dfLatitudeSpecies, 17))
-nrow(dfLatitude)
-# TEST 2: Does the trait have enough data variation?
-hist(dfLatitude$median_lat)
-range(dfLatitude$median_lat)
+dfLatitude <- setDT(GetTraitSpecificDataBIN(dfLatitudeSpecies, 16))
+# Get information for the trait.
+GetTraitInfo(dfLatitude, "median_lat", type = "continuous")
 
 # Datatable reorganization for dfFiltered.
-dfFiltered <- dfFiltered[, .(bin_uri, recordID, order_name = order_label, 
-                             family_name = family_label, 
-                             genus_name = genus_label, 
-                             species_name = species_label, nucleotides, 
-                             initial_bin_size, filtered_bin_size)]
+dfFiltered <- dfFiltered[, .(bin_uri, filtered_bin_size, recordID, order_name = order_label,
+                             family_name = family_label, genus_name = genus_label,
+                             species_name = species_label, nucleotides)]
 
 ################################################################################
 
@@ -106,11 +102,9 @@ dfBoldBase <- merge(dfFiltered, dfFishBaseSpecies, by = "species_name")
 # for first time (aka if you haven't saved trait info in your current working
 # directory (CWD) yet).
 speciesNames <- unique(dfBoldBase$species_name)
+speciesNames <- as.character(speciesNames)
 
 ### TRAIT ASSIGNMENT AND RECODING SECTION ###
-# As there are multiple entries per species for some traits, I want to take the 
-# median or mode value of some traits. This will depend on the type of trait 
-# i.e. continuous vs. categorical).
 # Note: Only some traits are shown here as examples.
 
 ### SPECIES TRAITS ###
@@ -123,12 +117,9 @@ dfSpecies <- fread("species_data.csv")
 # syntax consistent throughout the pipeline. Here, I am only extracting the 
 # trait data I am interested in from this datatable: longevity, body length,
 # and salinity.
-dfSpeciesTraits <- dfSpecies[, .(species_name = sciname, 
-                                 longevity = LongevityWild, 
-                                 max_length = Length, 
-                                 type_length = LTypeMaxM,
-                                 freshwater = Fresh,
-                                 saltwater = Saltwater,
+dfSpeciesTraits <- dfSpecies[, .(species_name = sciname, longevity = LongevityWild, 
+                                 max_length = Length, type_length = LTypeMaxM,
+                                 freshwater = Fresh, saltwater = Saltwater,
                                  brackish = Brack)]
 
 # Use the GetTraitSpecificData function to obtain trait data on a per species
@@ -136,26 +127,18 @@ dfSpeciesTraits <- dfSpecies[, .(species_name = sciname,
 # the trait of interest.
 
 # TRAIT: Longevity.
-# First, make sure it is a numeric variable so I can perform statistical tests 
-# on the data down the line.
-dfSpeciesTraits[, longevity := as.double(longevity)]
-# TEST 1: Does the trait have an adequate sample size?
+# Get the trait specific datatable.
 dfLongWild <- setDT(GetTraitSpecificData(dfSpeciesTraits, 2))
-nrow(dfLongWild)
-# TEST 2: Does the trait have enough data variation?
-hist(dfLongWild$longevity)
-range(dfLongWild$longevity)
+# Get the trait information.
+GetTraitInfo(dfLongWild, "longevity", type = "continuous")
 
 # TRAIT: Maximum length.
-dfSpeciesTraits[, max_length := as.double(max_length)]
-# We only want total length measurements.
+# We only want total length measurements for this trait.
 dfMaxLength <- dfSpeciesTraits[type_length == "TL"]
 # TEST 1: Does the trait have an adequate sample size?
 dfMaxLength <- setDT(GetTraitSpecificData(dfMaxLength, 3))
-nrow(dfMaxLength)
-# TEST 2: Does the trait have enough data variation?
-hist(dfMaxLength$max_length)
-range(dfMaxLength$max_length)
+# Get the trait information.
+GetTraitInfo(dfMaxLength, "max_length", type = "continuous")
 
 # TRAIT: Salinity.
 # I have to do some recoding for this trait. I only want purely saltwater,
@@ -177,99 +160,42 @@ dfSalinity <- dfSalinity[num_salinity_types == 1]
 # Now let's combine them into one "Salinity" variable.
 dfSalinity <- as.data.frame(dfSalinity)
 dfSalinity$salinity <- colnames(dfSalinity[2:4])[max.col(dfSalinity[2:4])]
-# TEST 1: Does the trait have an adequate sample size?
+# Get trait specific datatable.
 dfSalinity <- setDT(GetTraitSpecificData(dfSalinity, 6))
-nrow(dfSalinity)
-# TEST 2: Does the trait have enough data variation?
 # Convert to factor type.
 dfSalinity[, salinity := as.factor(salinity)]
-table(dfSalinity$salinity)
+# Get the trait information.
+GetTraitInfo(dfSalinity, "salinity", type = "discrete")
 # To remove a rare variable (e.g. brackish), you can use the following lines.
 #dfSalinity <- dfSalinity[!salinity == "brackish"]
 # Make sure to also drop the level from the factor.
 #dfSalinity[, salinity := droplevels(salinity)]
 
 # Finally, prepare the dfSpeciesGenMV datatable by merging all single variable
-# datatables. This datatable will be used for the eventual multivariable 
-# analysis.
+# datatables. This datatable will be used for the eventual multivariable analysis.
 # Merging multiple datatables at once.
 dfSpeciesGenMV <- Reduce(function(...) merge(..., all = T), 
                          list(dfLongWild, dfMaxLength, dfSalinity))
-
-### LIFE HISTORY RELATED ###
-# Maturity.
-dfMaturity <- data.frame(maturity(speciesNames))
-write.csv(dfMaturity, file = "maturity_data.csv") 
-dfMaturity <- fread("maturity_data.csv")
-# Datatable reorganization and renaming. Renaming column names to keep variable
-# syntax consistent throughout the pipeline.
-dfMaturityTraits <- dfMaturity[, .(species_name = sciname, 
-                                   age_at_maturity = tm)]
-
-# Median trait(s).
-# TRAIT: Age at maturity.
-# The column must first be converted to double (numeric) type.
-dfAgeMaturity <- dfMaturityTraits[, age_at_maturity := 
-                                    as.double(age_at_maturity)]
-# The median value is then determined for each species as there can be multiple
-# entries per species in this dataframe.
-dfAgeMaturity[, age_at_maturity := median(age_at_maturity, na.rm = TRUE), 
-              keyby = species_name]
-# TEST 1: Does the trait have an adequate sample size?
-dfAgeMaturity <- setDT(GetTraitSpecificData(dfAgeMaturity, 2))
-nrow(dfAgeMaturity)
-# TEST 2: Does the trait have enough data variation?
-hist(dfAgeMaturity$age_at_maturity)
-range(dfAgeMaturity$age_at_maturity) 
 
 ### ECOLOGY TRAITS ###
 dfEcology <- data.frame(ecology(speciesNames))
 write.csv(dfEcology, file = "ecology_data.csv") 
 dfEcology <- fread("ecology_data.csv")
-# Get rid of columns I do not need for the regression analysis. We are
-# just doing to experiment with a few traits right now.
 # Note: There is only one row per species in dfEcology.
-dfEcologyTraits <- dfEcology[, .(species_name = sciname, lakes = Lakes, 
-                                 streams = Stream)]
-# As some of the variables are coded as integers right now, we need to recode 
-# them to the types needed for regression analysis (i.e. factor type).
-# Which columns are integers?
-integerVars <- dfEcologyTraits[, sapply(.SD, is.integer)]
-integerVars <- which(integerVars == "TRUE")
-# Which columns are characters?
-characterVars <- dfEcologyTraits[, sapply(.SD, is.character)]
-characterVars <- which(characterVars == "TRUE")
-# Except for species_name.
-characterVars <- tail(characterVars, -1)
-# Combine integerVars and characterVars.
-changeVars <- c(integerVars, characterVars)
-# Change all of the character and integer columns to factor type.
-dfEcologyTraits[, (changeVars) := lapply(.SD, as.factor), .SDcols = changeVars]
+dfEcologyTraits <- dfEcology[, .(species_name = sciname, lakes = Lakes)]
+# Recode as factor.
+dfEcologyTraits[, lakes := as.factor(lakes)]
 # Recode the variables (from "-1" to "1").
-dfEcologyTraits[, (integerVars) := lapply(.SD, function(x) 
-  revalue(x, c("-1" = "1"))), .SDcols = integerVars]
+dfEcologyTraits[, lakes := revalue(lakes, c("-1" = "1"))]
 
-### Categorical traits. ###
 # Binary trait(s).
 # TRAIT: Lakes.
-# TEST 1: Does the trait have an adequate sample size?
+# Get the trait specific datatable.
 dfLakes <- setDT(GetTraitSpecificData(dfEcologyTraits, 2))
-nrow(dfLakes)
-# TEST 2: Does the trait have enough data variation?
-table(dfLakes$lakes)
+# Get trait information.
+GetTraitInfo(dfLakes, "lakes", type = "discrete")
 
-# TRAIT: Streams.
-# TEST 1: Does the trait have an adequate sample size?
-dfStreams <- setDT(GetTraitSpecificData(dfEcologyTraits, 3))
-nrow(dfStreams)
-# TEST 2: Does the trait have enough data variation?
-table(dfStreams$streams)
-
-# Finally, prepare the dfEcologyMV datatable by merging all single variable
-# datatables.
-dfEcologyMV <- merge(dfLakes, dfStreams, all = T)
-
-# Reproduction.
+### REPRODUCTION TRAIT(S) ###
 dfReproduction <- data.frame(reproduction(speciesNames))
 write.csv(dfReproduction, file = "reproduction_data.csv") 
 # Read in the reproduction information.
@@ -282,52 +208,44 @@ dfReproTraits <- dfReproTraits[!duplicated(species_name)]
 
 # Multi-level trait.
 # TRAIT: Parental Care.
-# TEST 1: Does trait have data for at least 500 species?
+# Get the trait specific datatable.
 dfParentalCare <- setDT(GetTraitSpecificData(dfReproTraits, 2))
-# TEST 2: Does the trait have enough data variation?
-table(dfParentalCare$parental_care)
-# If there are rare categories, remove them! For example:
-#dfParentalCare <- dfParentalCare[!parental_care == "biparental"]
-# Also dropping these levels from the factor.
+# Convert to factor.
 dfParentalCare[, parental_care := as.factor(parental_care)]
-dfParentalCare[, parental_care := droplevels(parental_care)]
+# Get trait information.
+GetTraitInfo(dfParentalCare, "parental_care", type = "discrete")
 
 # Construction of dfTraits datatable.
 # This table will contain all of the potential traits for multivariable 
-# analysis.NA/NULL/blank for those species that don't have info for that 
+# analysis. NA/NULL/blank for those species that don't have info for that 
 # particular trait.
 # Let's merge the trait information back to dfFiltered.
 # Note: I only want a single row per BIN from dfFiltered for this merging 
 # process (i.e. I just want information for the BIN URI, species name, and size
-# of the bin). bin_uri
+# of the bin).
 dfFilteredSingle <- dfFiltered[!duplicated(bin_uri)]
 # Let's take the columns we need to construct the dfTraits datatable.
-dfFilteredSingle <- dfFilteredSingle[, .(bin_uri, species_name, 
-                                         initial_bin_size, filtered_bin_size)]
+dfFilteredSingle <- dfFilteredSingle[, .(bin_uri, species_name, filtered_bin_size)]
 # Now merge to all of the single variable trait datatables.
 dfTraits <- merge(dfFilteredSingle, dfLatitude, all = T, by = "bin_uri")
 # Set the keys for datatable merging.
 setkey(dfTraits, species_name)
-setkey(dfSpeciesGenMV, species_name)
-setkey(dfEcologyMV, species_name)
-setkey(dfAgeMaturity, species_name)
+setkey(dfLakes, species_name)
 setkey(dfParentalCare, species_name)
 dfTraits <- Reduce(function(...) merge(..., all = T), list(dfTraits, 
                                                            dfSpeciesGenMV,
-                                                           dfEcologyMV,
-                                                           dfAgeMaturity,
+                                                           dfLakes,
                                                            dfParentalCare))
 # Dataframe reorganization.
-dfTraits <- dfTraits[, c(1:4, 7:14)]
-colnames(dfTraits)[4] <- "filtered_bin_size"
+dfTraits <- dfTraits[, c(1:3, 6:11)]
+setnames(dfTraits, "filtered_bin_size.x", "filtered_bin_size")
 # Remove species that don't have ANY trait information available.
-dfMissing <- dfTraits[apply(dfTraits[, c(5:12)], 1, function(x) all(is.na(x)))]
+dfMissing <- dfTraits[apply(dfTraits[, c(4:9)], 1, function(x) all(is.na(x)))]
 dfTraits <- dfTraits[!dfTraits$bin_uri %in% dfMissing$bin_uri]
 # Merge back to dfFiltered to obtain all of the sequence information for 
 # each BIN. This is for creation of the master phylogeny. 
 dfPreCentroid <- merge(dfFiltered, dfTraits, by = "bin_uri")
 # Dataframe reorganization and renaming.
-colnames(dfPreCentroid)[6] <- "species_name"
-colnames(dfPreCentroid)[8] <- "initial_bin_size"
-colnames(dfPreCentroid)[9] <- "filtered_bin_size"
-dfPreCentroid <- dfPreCentroid[, c(1:9)]
+setnames(dfPreCentroid, c("species_name.x", "filtered_bin_size.x"), 
+         c("species_name", "filtered_bin_size"))
+dfPreCentroid <- dfPreCentroid[, c(1:8)]
